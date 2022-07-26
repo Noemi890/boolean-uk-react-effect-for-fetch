d0a02ee8873d483a16e091a9797c4c3b1023a7aa
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-07-26 17:19:29 +0100

    refactoring

--- src/sections/Advice/components/AdviceSlip.js
+++ src/sections/Advice/components/AdviceSlip.js
@@ -1,13 +1,8 @@
-export const AdviceSlip = ({advices}) => {
+export const AdviceSlip = ({advice}) => {
   return (
     <>
       <h3>Some Advice</h3>
-      { advices.map(advice => {
-        return (
-          <p key={advice.id}>{advice.advice}</p>
-        )
-      })
-      }
+      <p>{advice?.advice}</p>
     </>
   )
 }
--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -7,32 +7,34 @@ const API = 'https://api.adviceslip.com/advice'
 
 function AdviceSection() {
 
-  const [advices, setAdvices] = useState([])
+  const [advice, setAdvice] = useState()
   const [preferredAdvice, setPreferredAdvice] = useState([])
-  const [handleClick, setHandleClick] = useState(false)
+  // const [handleClick, setHandleClick] = useState(false)
 
   const handleFavouritesClick = () => {
     const myCopy = preferredAdvice.slice()
-    const find = myCopy.find(item => item.id === advices[0].id)
-    if(find === undefined) myCopy.push(...advices)
+    const find = myCopy.find(item => item.id === advice.id)
+    if(find === undefined) myCopy.push(advice)
     else return
     setPreferredAdvice(myCopy)
   }
 
+  const getAdvice = async () => {
+      const resp = await fetch(API)
+      const data = await resp.json()
+      setAdvice(data.slip)
+  }
+
   useEffect(() => {
-    fetch(API)
-      .then(resp => resp.json())
-      .then(resp => {
-        setAdvices([resp.slip])
-      })
-  },[handleClick])
+    getAdvice()
+  },[])
 
   return (
     <section>
       <h2>Advice Section</h2>
       <section className="adivce-slip">
-        <AdviceSlip advices={advices}/>
-        <button onClick={() => setHandleClick(!handleClick)}>Get More Advice</button>
+        <AdviceSlip advice={advice}/>
+        <button onClick={getAdvice}>Get More Advice</button>
         <button onClick={handleFavouritesClick}>Save to Favourites</button>
       </section>
       <section className="favourtite-slips-list">
